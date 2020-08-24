5bd746f9ab1850d3d5b8613999e11ab6288b0143
maksymprudnik <maksymprudnik@gmail.com>
2020-08-25 02:19:48 +0300

    added profile post method to update user info

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const handleProfileGet = (req, res, db) => {
     const { id } = req.params;
     db.select('*').from('users').where({id})
@@ -11,6 +13,23 @@ const handleProfileGet = (req, res, db) => {
         .catch(err => res.status(400).json('Error getting user'));
 }
 
+const handleProfileUpdate = (req, res, db) => {
+    const { id } = req.params;
+    const { name, avatar } = req.body.formInput;
+    db('users')
+        .where({ id })
+        .update({ name, avatar })
+        .then(response => {
+            if(response) {
+                res.status(200).json('Success');
+            } else {
+                res.status(400).json('Unable to update');
+            }
+        })
+        .catch(err => res.status(400).json('Error updating user'));
+}
+
 module.exports = {
-    handleProfileGet
+    handleProfileGet,
+    handleProfileUpdate
 }
